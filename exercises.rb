b05e76089e4eee09e86f12b7f7930c31a046b2d4
Brittney Kistner <bkistner01@gmail.com>
2014-06-06 15:33:11 -0700

    Completed test and method for ex3

--- exercises.rb
+++ exercises.rb
@@ -22,7 +22,7 @@ module Exercises
   # Exercise 3
   #  - Returns the sum of the given array of numbers
   def self.ex3(array)
-    # TODO
+    array.inject{|sum, n| sum + n}
   end
 
   # Exercise 4
--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -2,9 +2,6 @@ require 'rubygems'
 require_relative '../exercises.rb'
 
 describe Exercises do
-
-  # let method?
-
   describe '.ex0' do
     context 'str is wishes' do
       it 'Triples a str' do
@@ -25,10 +22,22 @@ describe Exercises do
       expect(array).to eq(3)
     end
   end
-  describe '.ex1' do
+  describe '.ex2' do
     it 'returns the second element of array' do
       array = Exercises.ex2([1,2,3])
       expect(array).to eq(2)
     end
   end
+  describe '.ex3' do
+    it 'returns the sum of numbers in the array' do
+      array = Exercises.ex3([1,2,3])
+      expect(array).to eq(6)
+    end
+  end
+  # describe '.ex4' do
+  #   it 'returns the max number of the array' do
+  #     array = Exercises.ex4([1,2,3])
+  #     expect(array).to eq(3)
+  #   end
+  # end
 end
